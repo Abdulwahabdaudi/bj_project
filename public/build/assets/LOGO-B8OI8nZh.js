b44bb064c5c09dfc8f947b09a4ae7be3f9e1f52a
Abdulwahab Daudi <abdulwahabdaudi@gamil.com>
2025-06-10 11:48:28 +0300

    added build files

---
+++ public/build/assets/LOGO-B8OI8nZh.js
@@ -0,0 +1,21 @@
+import{E as a}from"./app-DjpV61ds.js";/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase();/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var o={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"};/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=({size:e,strokeWidth:t=2,absoluteStrokeWidth:s,color:r,iconNode:n,name:i,class:w,...l},{slots:c})=>a("svg",{...o,width:e||o.width,height:e||o.height,stroke:r||o.stroke,"stroke-width":s?Number(t)*24/Number(e):t,class:["lucide",`lucide-${d(i??"icon")}`],...l},[...n.map(u=>a(...u)),...c.default?[c.default()]:[]]);/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=(e,t)=>(s,{slots:r})=>a(h,{...s,iconNode:t,name:e},r),p="/build/assets/LOGO-BTc_0u0c.png";export{p as _,m as c};
